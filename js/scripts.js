b18f41b036844d87d0198af9801935103e5673b9
alvynah <alvynahw@gmail.com>
2021-03-06 09:06:15 +0300

    Add Deluxe and Classic forms for ordering pizza and a form for extra toppings

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,29 @@ $(document).ready(function() {
 
     function openNav() {
         document.getElementById("mySidenav").style.width = "150px";
-    }
+    };
 
     function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
-    }
+    };
+    $("#deluxeOrder").click(function() {
+        $("#deluxeOrder").hide();
+        $("#classicOrder").hide();
+        $("#deluxepizzaForm").show();
+    });
+    $("#classicOrder").click(function() {
+        $("#deluxeOrder").hide();
+        $("#classicOrder").hide();
+        $("#classicpizzaForm").show();
+    });
+    $("#deluxepizzatoppings-btn-submit").click(function(event) {
+        event.preventDefault();
+        $("#mediumToppings").show($("#deluxetoppings"));
+
+    });
+    $("#classicpizzatoppings-btn-submit").click(function(event) {
+        event.preventDefault();
+        $("#mediumToppings").show();
+    });
+
 });
